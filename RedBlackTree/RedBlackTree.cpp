b5217e4bd9c5fb7eeaa650d388b49aabe4b5aed4
bitate <1660513875@qq.com>
2020-07-05 16:44:32 +0800

    Add red black tree node struct

--- RedBlackTree/RedBlackTree.cpp
+++ RedBlackTree/RedBlackTree.cpp
@@ -1,6 +1,43 @@
-/*	1. A red-black tree is a binary search tree with nodes colored red and black
-	2. Root is black;
-	3. Every external node is black
-	4. The children of a red node are black
-	5. All the external nodes have the same black depth;
-*/
+/**
+ *	1. A red-black tree is a binary search tree with nodes colored red and black
+ *	2. Root is black;
+ *	3. Every external node is black;
+ *	4. The children of a red node are black;
+ *	5. All the external nodes have the same black depth;
+ */
+
+#include <iostream>
+
+using namespace std::cout;
+using namespace std::endl;
+
+enum class Color
+{
+	Red,
+	Black,
+};
+
+struct Node
+{
+	int value;
+	Color color;
+	struct Node* parent_node;
+	struct Node* left_node;
+	struct Node* right_node;
+};
+
+struct Node* nill_node;
+struct Node* root_node;
+
+void left_rotate(struct Node* current_node);
+void right_rotate(struct Node* current_node);
+void insert_value(int value);
+void delete_node(struct Node* current_node);
+
+
+
+int main()
+{
+
+}
+
