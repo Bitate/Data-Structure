abb34b95aa281f242004b1fe721f2762f9aa87a1
bitate <1660513875@qq.com>
2020-07-07 21:18:07 +0800

    Add red-black right and left rotation functions

--- RedBlackTree/RedBlackTree.cpp
+++ RedBlackTree/RedBlackTree.cpp
@@ -8,8 +8,7 @@
 
 #include <iostream>
 
-using namespace std::cout;
-using namespace std::endl;
+using namespace std;
 
 enum class Color
 {
@@ -29,15 +28,56 @@ struct Node
 struct Node* nill_node;
 struct Node* root_node;
 
-void left_rotate(struct Node* current_node);
-void right_rotate(struct Node* current_node);
-void insert_value(int value);
-void delete_node(struct Node* current_node);
-
-
+void rotate_left(struct Node* new_node, struct Node* root_node);
+void rotate_right(struct Node* new_node, struct Node* root_node);
 
 int main()
 {
 
 }
 
+void rotate_left(struct Node* new_node, struct Node* root_node)
+{
+	struct Node* right_child_of_new_node = new_node->right_node;
+
+	// step 1: let left child of right_child_of_new_node be the right child of new_node
+	new_node->right_node = right_child_of_new_node->left_node;
+	if(right_child_of_new_node->left_node != nullptr)
+		right_child_of_new_node->left_node->parent_node = new_node;
+	
+	// step 2: let right_child_of_new_node be the child(right or left) of new_node's parent.
+	right_child_of_new_node->parent_node = new_node->parent_node;
+	if(new_node == root_node)
+		root_node == right_child_of_new_node;
+	else if(new_node == new_node->parent_node->left_node)
+		new_node->parent_node->left_node = right_child_of_new_node;
+	else
+		new_node->parent_node->right_node = right_child_of_new_node;
+
+	// step 3: let new_node be the left child of right_child_of_new_node
+	right_child_of_new_node->left_node = new_node;
+	new_node->parent_node = right_child_of_new_node;
+}
+
+void rotate_right(struct Node* new_node, struct Node* root_node)
+{
+	struct Node* left_child_of_new_node = new_node->left_node;
+
+	// step 1: let right child of left_child_of_new_node be 
+	new_node->left_node = left_child_of_new_node->right_node;
+	if(left_child_of_new_node->right_node != nullptr)
+		left_child_of_new_node->right_node->parent_node = new_node;
+
+	// step 2: let left_child_of_new_node be the child(left or right) of new_node's parent.
+	left_child_of_new_node->parent_node = new_node->parent_node;
+	if(new_node == root_node)
+		root_node = left_child_of_new_node;
+	else if(new_node == new_node->parent_node->left_node)
+		new_node->parent_node->left_node = left_child_of_new_node;
+	else
+		new_node->parent_node->right_node = left_child_of_new_node;
+	
+	// step 3: let new_node be the right node of left_child_of_new_node.
+	left_child_of_new_node->right_node = new_node;
+	new_node->parent_node = left_child_of_new_node;
+}
